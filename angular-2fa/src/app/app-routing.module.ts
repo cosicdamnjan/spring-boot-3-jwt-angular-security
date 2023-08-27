b45ee41dc4a3c6436a47e614ee9f6bf23f437b95
Damnjan <cosicdamnjan@gmail.com>
2023-08-27 15:27:18 +0200

    Finished frontend

--- angular-2fa/src/app/app-routing.module.ts
+++ angular-2fa/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./pages/login/login.component";
+import {RegisterComponent} from "./pages/register/register.component";
+import {WelcomeComponent} from "./pages/welcome/welcome.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'welcome',
+    component: WelcomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
