ab84959719c55534ec2cdf271893cc61923f4826
Damnjan <cosicdamnjan@gmail.com>
2023-08-27 12:25:21 +0200

    Initial commit

---
+++ src/test/java/com/damnjan/security/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damnjan.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
