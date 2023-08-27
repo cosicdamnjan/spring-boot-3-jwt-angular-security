b87db9072f2a530e88bbbf00e0fd57d2fcb756e4
Damnjan <cosicdamnjan@gmail.com>
2023-08-27 13:34:33 +0200

    Added angular project

---
+++ src/main/java/com/damnjan/security/auth/VerificationRequest.java
@@ -0,0 +1,12 @@
+package com.damnjan.security.auth;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class VerificationRequest {
+
+    private String email;
+    private String code;
+}
